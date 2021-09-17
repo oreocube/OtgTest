4882763954887b4c595a844e78d8d08de7c8bb33
oreocube <oreocube13@gmail.com>
2021-09-18 02:40:52 +0900

    실시간으로 위치 정보 받기

--- app/src/main/java/com/shareyacht/otgtest/MainActivity.kt
+++ app/src/main/java/com/shareyacht/otgtest/MainActivity.kt
@@ -1,21 +1,27 @@
 package com.shareyacht.otgtest
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
-import android.hardware.usb.UsbManager
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
 import android.text.method.ScrollingMovementMethod
+import android.util.Log
 import android.view.View
-import com.hoho.android.usbserial.driver.UsbSerialPort
-import com.hoho.android.usbserial.driver.UsbSerialProber
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.*
 import com.shareyacht.otgtest.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
 private const val ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION"
-private const val TAG = "MainActivity"
+private const val TAG = "로그"
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -26,11 +32,19 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var mSerialConnector: SerialConnector
     private lateinit var mBinding: ActivityMainBinding
 
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+    private var currentLocation: Location? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mBinding = ActivityMainBinding.inflate(layoutInflater)
+
+        // 앱 실행 시 위치 권한 얻기
+        checkLocationPermission()
 
         mContext = applicationContext
 
@@ -48,12 +62,80 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         mSerialConnector = SerialConnector(mContext, mListener, mHandler)
         mSerialConnector.initialize()
+
+
     }
 
     override fun onDestroy() {
         super.onDestroy()
-
         mSerialConnector.finalize()
+    }
+
+    private fun checkLocationPermission() {
+        Log.d(TAG, "MainActivity - checkLocationPermission() called")
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+                    && ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // You can use the API that requires the permission.
+                // 권한이 허용되어 있는 경우
+                Log.d(TAG, "퍼미션 허용되어 있음")
+
+                fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+                locationRequest = LocationRequest.create().apply {
+                    interval = TimeUnit.SECONDS.toMillis(2)
+                    fastestInterval = TimeUnit.SECONDS.toMillis(1)
+                    maxWaitTime = TimeUnit.SECONDS.toMillis(3)
+
+                    priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+                }
+
+                locationCallback = object : LocationCallback() {
+                    override fun onLocationResult(locationResult: LocationResult) {
+                        super.onLocationResult(locationResult)
+                        val now = System.currentTimeMillis()
+                        val date = Date(now)
+                        val sdf = SimpleDateFormat("MM/dd hh:mm:ss", Locale.KOREA)
+                        currentLocation = locationResult.lastLocation
+
+                        Log.d(TAG, sdf.format(date))
+                        Log.d(TAG, "${currentLocation!!.latitude}//${currentLocation!!.longitude}")
+                    }
+                }
+
+                fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                Log.d(TAG, "ACCESS_FINE_LOCATION 퍼미션 필요")
+                requestPermissions(
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ), 1000
+                )
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_COARSE_LOCATION) -> {
+                Log.d(TAG, "ACCESS_COARSE_LOCATION 퍼미션 필요")
+                requestPermissions(
+                    arrayOf(
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ), 1000
+                )
+            }
+            else -> {
+                requestPermissions(
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ), 1000
+                )
+            }
+        }
     }
 
     override fun onClick(v: View) {
